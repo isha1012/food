86665d26b61df125e773cbab700127f3e2a90d78
unknown <aishafehintola@gmail.com>
2020-01-11 02:36:12 +0100

    new files

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>eatery</title>
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
+<link href="styles.css" rel="stylesheet" type="text/css" />
+<link href="font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="skdslider-master/src/skdslider.min.js"></script>  
+<link href="skdslider-master/src/skdslider.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	
+	<div class="first_header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4">
+                        <div class="details ">
+                            <a href="#"><i class="fa fa-phone"></i>08108090811</a>
+                            <a href="#"><i class="fa fa-envelope-o"></i>08108090811</a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-lg-4">
+                        <div class="event_btn_inner">
+                            <a class="event_btn" href="table.html"><i class="fa fa-table" aria-hidden="true"></i>&nbsp;Book a Table</a>
+                            <a id="book" href="event.html"><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;Book an Event</a>
+                        </div>
+                    </div>
+                     <div class="col-md-4 col-lg-4">
+                        <div class="header_social">
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <nav class="navbar">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu" aria-controls="bs-navbar" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        </button>
+                        <div id="logo"><a class="navbar-brand " href="#"><b>RED</b><h3>RESTAURANT</h3></a></div>
+                    </div>
+                    <div class="collapse navbar-collapse "id="menu">
+                        <ul class="nav navbar-nav pull-right">
+                            <li class="active"><a href="index.html">Home</a></li>
+                                    <li><a href="about-us.html">About Us</a></li>
+                            <li class="dropdown submenu">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="menu-grid.html">Menu Grid</a></li>
+                                    <li><a href="menu-list.html">Menu List</a></li>
+                                    <li><a href="#">Today's Menu</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="gallery.html">Gallery</a></li>
+                            <li class="dropdown submenu">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="event.html">Event</a></li>
+                                    <li><a href="table.html">Table</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown submenu">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">News <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="blog.html">Blog</a></li>
+                                    <li><a href="blog-gallery.html">Blog Gallery</a></li>
+                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="contact.html">Contact US</a></li>
+                            <li><a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div>
+              </div>
+        </nav>
+    		
+   			 <div id="skip" >
+      <div class="slide">
+        <img src="img/image4.jpg" />
+        <!--Slider Description example-->
+         <div class="slide-desc">
+            <h2>Slider Title 1</h2>
+            <p>Demo description here. Demo description here. Demo description here. Demo description here. Demo description here. <a class="more" href="#">more</a></p>
+        </div>
+     </div>
+    <div class="slide">
+       <img src="img/image.jpg"/>
+       <div class="slide-desc">
+        <h2>Slider Title 2</h2>
+        <p>Demo description here. Demo description here. Demo description here. Demo description here. Demo description here. <a class="more" href="#">more</a></p>
+      </div>
+   </div>
+   <div class="slide">
+      <img src="img/image3.jpg"/>
+      <div class="slide-desc">
+        <h2>Slider Title 2</h2>
+        <p>Demo description here. Demo description here. Demo description here. Demo description here. Demo description here. <a class="more" href="#">more</a></p>
+      <!--NO Description Here-->
+   </div>
+   </div>	
+   </div>                 
+          	<div class="container">
+          		<div class="row">
+                <div class="col-lg-4">
+						<a href="#"><div class=" col-lg-1 col-md-1 " ></div></a>
+                    	<a href="#"><div class=" col-lg-1 col-md-1 " ></div></a> 
+                    </div>
+                	<div class="col-lg-4">
+                      <div id="serve">
+						<div class="col-lg-1 col-md-1"><a href="#"></a></div>
+                    	<div class="order col-lg-4 col-md-4"><a href="#">Order now</a></div>
+                        <div class="reserve col-lg-4 col-md-4"><a href="#">Reserve a table</a></div> 
+                      </div>
+                    </div>
+                    <div class="col-lg-4">
+						<a href="#"><div class="col-lg-6 col-md-6" ></div></a>
+                    	<a href="#"><div class="col-lg-6 col-md-6" ></div></a> 
+                    </div>
+       			 </div>         
+			</div>
+         </div>
+         <div id="content"></div>
+         
+         <div id="footer">
+         </div>
+         <div id="move">
+         <div id="footer_area">
+         	<div class="container">
+            	<div class="col-md-3 col-lg-3">
+            		<b>ABOUT RED RESTAURANT</b>
+                	<P>Lorem ipsum dolor sit amet, consectur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ut.</P>
+                    <div id="face">
+                    	<ul>
+                    		<li><a href="#"><i class="fa fa-facebook "></i></a></li>
+                    		<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    		<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                    		<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                     </div>
+           		</div>
+                <div class="col-md-3 col-lg-3">
+                	<b>CONTACT US</b>
+                    <p>Have questions, comments or just want to say hello:</p>
+                    <a href="#"><i class="fa fa-envelope-o"></i>backpiper.com@gmail.com</a>
+                    <a href="#"><i class="fa fa-phone"></i>08108090811</a>
+                    <a href="#"><i class="fa fa-map-marker"></i>5001 E. Colorado Blvd. Suite 820,
+Pasadena, CA 91106</a>
+                </div>
+                <div class="col-lg-3 col-md-3">
+                	<b>TWITTER FEED</b>
+                    <p><a href="#">@_sumonrahman:</a> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</p>
+                    <p><a href="#">@_sumonrahman:</a> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</p>
+                </div>
+                <div class="col-md-3 col-lg-3">
+                </div>
+                </div>
+            </div>
+            <div class="foot">
+            	<div id="log">copyright all right reserved</div>
+                <div id="nav">
+                	<ul>
+                    	<li><a href="#">Home</a></li>
+                        <li><a href="#">about us</a></li>
+                        <li><a href="#">menu</a></li>
+                        <li><a href="#">gallery</a></li>
+                        <li><a href="#">reservation</a></li>
+                        <li><a href="#">news</a></li>
+                        <li><a href="#">contact us</a></li>
+                    </ul>
+                </div>
+            </div>
+         </div>
+</div>
+<script type="text/javascript">
+    jQuery('#skip').skdslider({
+          slideSelector: '.slide',
+          delay:5000, 
+          animationSpeed: 1000,
+          showNextPrev:true,
+          showPlayButton:false,
+          autoSlide:true,
+          animationType:'sliding'
+        });
+
+</script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ stockarray.php
@@ -0,0 +1,26 @@
+<?php
+	$stockarray = array(
+		array("chicken fried rice",1200,"true","","","img18"),
+		array("Corn beans",800,"true","","","img8"),
+		array("spagetti beef",1200,"true","","","img15"),
+		array("white rice with beef and plaintain",1200,"true","","","img17"),
+		array("chicken fried rice",1200,"true","","","img2"),
+		array("chicken fried rice",1200,"true","","","img4"),
+		array("chicken fried rice",1200,"true","","","img16"),
+		array("chicken fried rice",1200,"true","","","img10"),
+	
+	
+	
+	
+	
+	
+	
+	);
+
+
+
+
+
+
+
+?>
